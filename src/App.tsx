a4183b0cc326413a63c99d82b7c0eec71ae8abde
princend <waxdji3g4y3c04@gmail.com>
2025-04-02 16:15:45 +0800

    init project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React, { JSX  } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App(): JSX.Element {
+  return (
+    <div>
+      <header>
+      </header>
+      <body>
+        123
+      </body>
+    </div>
+  );
+}
+
+export default App;
